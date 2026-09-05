ffca726834f077d53f7ff3f830573679c9acecaf
Z-HongYuan <1939218189@qq.com>
2026-09-06 02:30:13 +0800

    新增 OmniEditorEngine 并配置内容浏览器显示插件文件夹

---
+++ Source/OmniEditor/Private/System/OmniEditorEngine.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright © 2026 张鸿源. All Rights Reserved.
+
+
+#include "System/OmniEditorEngine.h"
+
+#include "OmniEditor/OmniEditorLogChannel.h"
+#include "Settings/ContentBrowserSettings.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(OmniEditorEngine)
+
+void UOmniEditorEngine::Init(IEngineLoop* InEngineLoop)
+{
+	Super::Init(InEngineLoop);
+
+	UE_LOG(LogOmniEditor, Log, TEXT("OmniEditorEngine Init"));
+
+	// 加载时强制显示插件内容。
+	GetMutableDefault<UContentBrowserSettings>()->SetDisplayPluginFolders(true);
+}
---
+++ Source/OmniEditor/Public/System/OmniEditorEngine.h
@@ -0,0 +1,21 @@
+﻿// Copyright © 2026 张鸿源. All Rights Reserved.
+
+#pragma once
+
+#include "Editor/UnrealEdEngine.h"
+#include "OmniEditorEngine.generated.h"
+
+#define UE_API OMNIEDITOR_API
+
+/**
+ * 项目编辑器使用的引擎类
+ */
+UCLASS(MinimalAPI)
+class UOmniEditorEngine : public UUnrealEdEngine
+{
+	GENERATED_BODY()
+
+public:
+	UE_API virtual void Init(IEngineLoop* InEngineLoop) override;
+};
+#undef UE_API
