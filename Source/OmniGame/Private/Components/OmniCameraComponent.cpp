43114d9d68a7d30b59dd2960f48dd55a4607bba3
Z-HongYuan <1939218189@qq.com>
2026-09-07 17:35:26 +0800

    新增 OmniCameraComponent 相机组件，将相机初始化接入 Pawn 状态链，从 PawnData 读取相机资产并在本地控制端激活相机系统

---
+++ Source/OmniGame/Private/Components/OmniCameraComponent.cpp
@@ -0,0 +1,156 @@
+﻿// Copyright © 2026 张鸿源. All Rights Reserved.
+
+
+#include "Components/OmniCameraComponent.h"
+
+#include "Components/ExperiencePawnExtensionComponent.h"
+#include "Components/GameFrameworkComponentManager.h"
+#include "Data/ExperiencePawnData.h"
+#include "Data/ExperienceSystemTags.h"
+#include "GameFramework/GameplayCameraComponent.h"
+#include "OmniGame/OmniGameLogChannel.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(OmniCameraComponent)
+
+const FName UOmniCameraComponent::NAME_ActorFeatureName(TEXT("CameraManager"));
+const FName UOmniCameraComponent::NAME_CameraReady(TEXT("CameraReady"));
+
+UOmniCameraComponent::UOmniCameraComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	SetIsReplicatedByDefault(false);
+
+	// 引擎的相机组件负责真正的相机运行
+	GameplayCameraComponent = ObjectInitializer.CreateDefaultSubobject<UGameplayCameraComponent>(this, TEXT("GameplayCameraComponent"));
+}
+
+void UOmniCameraComponent::OnRegister()
+{
+	Super::OnRegister();
+
+	ensureAlwaysMsgf((GetPawn<APawn>() != nullptr), TEXT("UOmniCameraComponent 只能挂到 Pawn 上，当前所有者 [%s] 不是 Pawn。"), *GetNameSafe(GetOwner()));
+
+	RegisterInitStateFeature();
+}
+
+void UOmniCameraComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BindOnActorInitStateChanged(UExperiencePawnExtensionComponent::NAME_ActorFeatureName, FGameplayTag(), false);
+
+	ensure(TryToChangeInitState(ExperienceSystemTags::TAG_InitState_Spawned));
+	CheckDefaultInitialization();
+}
+
+void UOmniCameraComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UnregisterInitStateFeature();
+
+	Super::EndPlay(EndPlayReason);
+}
+
+bool UOmniCameraComponent::CanChangeInitState(UGameFrameworkComponentManager* Manager, FGameplayTag CurrentState, FGameplayTag DesiredState) const
+{
+	check(Manager);
+
+	APawn* Pawn = GetPawn<APawn>();
+
+	// 无状态 → Spawned：只要 Pawn 存在
+	if (!CurrentState.IsValid() && DesiredState == ExperienceSystemTags::TAG_InitState_Spawned)
+	{
+		if (Pawn) return true;
+	}
+	// Spawned → DataAvailable：PawnExtension 的 PawnData 就绪即可（相机不需要控制器）
+	if (CurrentState == ExperienceSystemTags::TAG_InitState_Spawned && DesiredState == ExperienceSystemTags::TAG_InitState_DataAvailable)
+	{
+		const UExperiencePawnExtensionComponent* PawnExtComp = UExperiencePawnExtensionComponent::FindPawnExtensionComponent(Pawn);
+		return (PawnExtComp != nullptr) && (PawnExtComp->GetPawnData<UExperiencePawnData>() != nullptr);
+	}
+	// DataAvailable → DataInitialized：等 PawnExtension 先完成
+	if (CurrentState == ExperienceSystemTags::TAG_InitState_DataAvailable && DesiredState == ExperienceSystemTags::TAG_InitState_DataInitialized)
+	{
+		return Manager->HasFeatureReachedInitState(Pawn, UExperiencePawnExtensionComponent::NAME_ActorFeatureName, ExperienceSystemTags::TAG_InitState_DataInitialized);
+	}
+	// DataInitialized → GameplayReady
+	if (CurrentState == ExperienceSystemTags::TAG_InitState_DataInitialized && DesiredState == ExperienceSystemTags::TAG_InitState_GameplayReady)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void UOmniCameraComponent::HandleChangeInitState(UGameFrameworkComponentManager* Manager, FGameplayTag CurrentState, FGameplayTag DesiredState)
+{
+	// 设置相机资产：本地控制端才会用到相机
+	if (CurrentState == ExperienceSystemTags::TAG_InitState_DataAvailable && DesiredState == ExperienceSystemTags::TAG_InitState_DataInitialized)
+	{
+		APawn* Pawn = GetPawn<APawn>();
+		if (Pawn && Pawn->IsLocallyControlled() && GameplayCameraComponent)
+		{
+			if (UCameraAsset* CameraAsset = GetCameraAssetFromPawnData())
+			{
+				// SetCameraAsset 是内联函数，内部调用已导出的 RebuildParameters()，跨模块调用安全
+				GameplayCameraComponent->CameraReference.SetCameraAsset(CameraAsset);
+			}
+			else
+			{
+				UE_LOG(LogOmniGame, Warning, TEXT("[%s] PawnData 未配置 CameraAsset，相机组件将使用 GameplayCameraComponent 上已配置的资产。"), *GetNameSafe(Pawn));
+			}
+		}
+	}
+
+	// 激活相机：等一切就绪
+	if (CurrentState == ExperienceSystemTags::TAG_InitState_DataInitialized && DesiredState == ExperienceSystemTags::TAG_InitState_GameplayReady)
+	{
+		APawn* Pawn = GetPawn<APawn>();
+		if (Pawn && Pawn->IsLocallyControlled() && GameplayCameraComponent)
+		{
+			if (APlayerController* PC = GetController<APlayerController>())
+			{
+				// bSetAsViewTarget=true 会把本组件设为视角目标
+				GameplayCameraComponent->ActivateCameraForPlayerController(PC);
+
+				OnCameraReady(GetCameraAssetFromPawnData());
+
+				// 广播相机就绪，给 GameFeature Action 挂相机相关内容（如持久相机 Rig、相机层）的时机
+				// 对应 Lyra 的 NAME_BindInputsNow 模式，发给 PC 和 Pawn 两个接收者
+				UGameFrameworkComponentManager::SendGameFrameworkComponentExtensionEvent(PC, NAME_CameraReady);
+				UGameFrameworkComponentManager::SendGameFrameworkComponentExtensionEvent(Pawn, NAME_CameraReady);
+			}
+		}
+	}
+}
+
+void UOmniCameraComponent::OnActorInitStateChanged(const FActorInitStateChangedParams& Params)
+{
+	check(Params.FeatureName == UExperiencePawnExtensionComponent::NAME_ActorFeatureName);
+
+	if (Params.FeatureState == ExperienceSystemTags::TAG_InitState_DataInitialized)
+	{
+		CheckDefaultInitialization();
+	}
+}
+
+void UOmniCameraComponent::CheckDefaultInitialization()
+{
+	// 感觉没必要推进
+	// CheckDefaultInitializationForImplementers();
+
+	ContinueInitStateChain(ExperienceSystemTags::ComponentStateChain);
+}
+
+UCameraAsset* UOmniCameraComponent::GetCameraAssetFromPawnData() const
+{
+	const UExperiencePawnExtensionComponent* PawnExtComp = UExperiencePawnExtensionComponent::FindPawnExtensionComponent(GetPawn<APawn>());
+	const UExperiencePawnData* PawnData = PawnExtComp ? PawnExtComp->GetPawnData<UExperiencePawnData>() : nullptr;
+
+	return PawnData ? PawnData->CameraAsset : nullptr;
+}
+
+void UOmniCameraComponent::OnCameraReady(UCameraAsset* CameraAsset)
+{
+	// 空实现，供派生类重载
+}
---
+++ Source/OmniGame/Public/Components/OmniCameraComponent.h
@@ -0,0 +1,70 @@
+﻿// Copyright © 2026 张鸿源. All Rights Reserved.
+
+#pragma once
+
+#include "Components/GameFrameworkInitStateInterface.h"
+#include "Components/PawnComponent.h"
+#include "OmniCameraComponent.generated.h"
+
+#define UE_API OMNIGAME_API
+
+class UCameraAsset;
+class UExperiencePawnExtensionComponent;
+class UGameplayCameraComponent;
+
+/**
+ * 项目的相机组件（对应 Lyra 的 HeroComponent 中相机部分 + ULyraCameraComponent）
+ *
+ * 职责：
+ * - 参与 Pawn 的初始化状态链，内部持有一个引擎的 UGameplayCameraComponent
+ * - DataInitialized 时从 PawnData 取相机资产，设置到 UGameplayCameraComponent::CameraReference
+ * - GameplayReady 且本地控制端时，激活相机系统
+ *
+ * 注意：相机只在本地控制端有意义，Bot 上本组件会正常推进状态链但不会激活相机。
+ * 相机的状态切换（第三人称/瞄准/倒地等）请在 UCameraAsset 内部用 CameraDirector 配置，
+ * 不要在这里做多相机资产的切换。
+ */
+UCLASS(MinimalAPI)
+class UOmniCameraComponent : public UPawnComponent, public IGameFrameworkInitStateInterface
+{
+	GENERATED_BODY()
+
+public:
+	UE_API UOmniCameraComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UE_API virtual void OnRegister() override;
+	UE_API virtual void BeginPlay() override;
+	UE_API virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	//~IGameFrameworkInitStateInterface interface
+	static UE_API const FName NAME_ActorFeatureName;
+	static UE_API const FName NAME_CameraReady;
+	virtual FName GetFeatureName() const override { return NAME_ActorFeatureName; }
+	UE_API virtual bool CanChangeInitState(UGameFrameworkComponentManager* Manager, FGameplayTag CurrentState, FGameplayTag DesiredState) const override;
+	UE_API virtual void HandleChangeInitState(UGameFrameworkComponentManager* Manager, FGameplayTag CurrentState, FGameplayTag DesiredState) override;
+	UE_API virtual void OnActorInitStateChanged(const FActorInitStateChangedParams& Params) override;
+	UE_API virtual void CheckDefaultInitialization() override;
+	//~End of IGameFrameworkInitStateInterface interface
+
+	// 获取内部的引擎相机组件
+	UFUNCTION(BlueprintPure, Category = "Omni|Camera")
+	UGameplayCameraComponent* GetGameplayCameraComponent() const { return GameplayCameraComponent; }
+
+	// 从当前 PawnData 读取相机资产（可能为空）
+	UFUNCTION(BlueprintPure, Category = "Omni|Camera")
+	UE_API UCameraAsset* GetCameraAssetFromPawnData() const;
+
+protected:
+	// 相机就绪钩子（本地控制端才会调用），派生类可重载
+	UE_API virtual void OnCameraReady(UCameraAsset* CameraAsset);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Omni|Camera", meta = (DisplayName = "OnCameraReady"))
+	UE_API void K2_OnCameraReady(UCameraAsset* CameraAsset);
+
+private:
+	// 引擎的相机组件，负责实际的相机运行
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Omni|Camera", Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UGameplayCameraComponent> GameplayCameraComponent;
+};
+
+#undef UE_API
