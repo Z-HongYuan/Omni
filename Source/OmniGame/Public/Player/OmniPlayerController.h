8bbe89169b680dcd2965a09acdbae953278e70fd
Z-HongYuan <1939218189@qq.com>
2026-09-07 17:25:22 +0800

    新增玩家控制器的ASC获取与每帧能力输入处理

--- Source/OmniGame/Public/Player/OmniPlayerController.h
+++ Source/OmniGame/Public/Player/OmniPlayerController.h
@@ -1,4 +1,4 @@
-﻿// Copyright © 2026 张鸿源. All Rights Reserved.
+// Copyright © 2026 张鸿源. All Rights Reserved.
 
 #pragma once
 
@@ -7,6 +7,8 @@
 
 #define UE_API OMNIGAME_API
 
+class UCustomAbilitySystemComponent;
+
 /**
  * 项目使用的默认玩家控制器类
  */
@@ -14,5 +16,16 @@ UCLASS(MinimalAPI)
 class AOmniPlayerController : public AExtensionPlayerController
 {
 	GENERATED_BODY()
+
+public:
+	UE_API AOmniPlayerController(const FObjectInitializer& ObjectInitializer);
+
+	// 获取玩家状态上持有的 ASC, 玩家状态未就绪时返回空(对标 LyraPlayerController::GetLyraAbilitySystemComponent)
+	UE_API UCustomAbilitySystemComponent* GetCustomAbilitySystemComponent() const;
+
+	// 每帧把积累的能力输入交给 ASC 处理
+	UE_API virtual void PostProcessInput(const float DeltaTime, const bool bGamePaused) override;
+
+protected:
 };
 #undef UE_API
