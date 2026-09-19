44e3dc226b73b63b7e4c39b762b3a55b0eb3e421
Z-HongYuan <1939218189@qq.com>
2026-09-19 15:56:18 +0800

    清空魂斗罗玩法，保留基础 GF 插件

--- Plugins/GameFeatures/OmniContra/Source/OmniContraRuntime/OmniContraRuntime.Build.cs
+++ Plugins/GameFeatures/OmniContra/Source/OmniContraRuntime/OmniContraRuntime.Build.cs
@@ -5,14 +5,6 @@ public class OmniContraRuntime : ModuleRules
 	public OmniContraRuntime(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		PublicDependencyModuleNames.AddRange(new[]
-		{
-			"Core", "CoreUObject", "Engine", "OmniGame", "AbilityExtension",
-			"GameplayAbilities", "ModularGameplay", "UMG"
-		});
-		PrivateDependencyModuleNames.AddRange(new[]
-		{
-			"ExperienceSystem", "EnhancedInput", "InputCore", "GameplayTags", "GameplayTasks", "SlateCore"
-		});
+		PrivateDependencyModuleNames.Add("Core");
 	}
 }
